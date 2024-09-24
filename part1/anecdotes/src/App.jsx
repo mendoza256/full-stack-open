5627ab9afe8da0877894123ab131539de0a32086
Christian Graumann <christian.graumann@10xinnovation.de>
2024-09-24 15:54:30 +0200

    refactor into components, functions, data

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,46 +1,38 @@
 import { useState } from "react";
+import Headline from "./components/Headline";
+import { returnHighestValueIndex, returnRandomIndex } from "./lib/utils";
+import Anecdote from "./components/Anecdote";
+import Button from "./components/Button";
+import anecdotes from "./data/anecdotes";
 
 const App = () => {
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
-  ];
-
   const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState({
-    ...new Array(anecdotes.length).fill(0),
-  });
-  const currentHighest = Object.values(votes);
-  const currentHighestIndex = currentHighest.indexOf(
-    Math.max(...currentHighest)
-  );
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+  const currentHighestValueIndex = returnHighestValueIndex(votes);
 
   function handleRandomizeQuote() {
-    const randomNumber = Math.floor(Math.random() * anecdotes.length);
-    setSelected(randomNumber);
+    const randomIndex = returnRandomIndex(anecdotes);
+    setSelected(randomIndex);
   }
 
-  function handleVote() {
-    const votesCopy = { ...votes };
-    votesCopy[selected] = votesCopy[selected] + 1;
+  function voteForAnecdote() {
+    const votesCopy = [...votes];
+    votesCopy[selected] += 1;
     setVotes(votesCopy);
   }
 
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <h2>Anecdote of the day</h2>
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-      <button onClick={handleRandomizeQuote}>Next anecdote</button>
-      <button onClick={handleVote}>Vote</button>
-      <h2>Anecdote with most votes</h2>
-      <p>{anecdotes[currentHighestIndex]}</p>
+    <div>
+      <Headline text="Anecdote of the day" />
+      <Anecdote anecdotes={anecdotes} selected={selected} votes={votes} />
+      <Button handleClick={handleRandomizeQuote} text="Next anecdote" />
+      <Button handleClick={voteForAnecdote} text="Vote" />
+      <Headline text="Anecdote with most votes" />
+      <Anecdote
+        anecdotes={anecdotes}
+        selected={currentHighestValueIndex}
+        votes={votes}
+      />
     </div>
   );
 };
---
+++ part1/anecdotes/src/components/Headline.jsx
@@ -0,0 +1,5 @@
+const Headline = ({ text }) => {
+  return <h2>{text}</h2>;
+};
+
+export default Headline;
