eeaa0d402cacd0a0f28c563e8be97491ce1be919
Christian Graumann <christian.graumann@10xinnovation.de>
2024-09-26 18:19:17 +0200

    part 2 progress

--- part2/phonebook/src/lib/utils.js
+++ part2/phonebook/src/lib/utils.js
@@ -1,7 +1,9 @@
 export function personIsInSearch(person, search) {
-    return person.name.toLowerCase().includes(search.toLowerCase())
+  return person.name.toLowerCase().includes(search.toLowerCase());
 }
 
 export function filterPersonsWithSearch(persons, search) {
+  if (persons.length > 0) {
     return persons.filter((person) => personIsInSearch(person, search));
+  }
 }
--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -1,13 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import axios from "axios";
-
-const promise = axios.get("http://localhost:3001/notes");
-console.log("promise", promise);
-
-const promise2 = axios.get("http://localhost:3001/persons");
-console.log("promise2", promise2);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
